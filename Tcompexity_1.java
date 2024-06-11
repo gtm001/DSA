92e20e31c8a490ccfd6517bee28f65dbd25e5d58
Gautam Kumar <119605352+gtm001@users.noreply.github.com>
2024-06-11 11:07:05 +0530

    Update Tcompexity_1.java

--- Tcompexity_1.java
+++ Tcompexity_1.java
@@ -53,36 +53,36 @@
  * 
  */
 
-import java.util.*;
+// import java.util.*;
 
-class Solution {
-    public List<String> fizzBuzz(int n) {
-        List<String> result = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                result.add("FizzBuzz");
-            } else if (i % 3 == 0) {
-                result.add("Fizz");
-            } else if (i % 5 == 0) {
-                result.add("Buzz");
-            } else {
-                result.add(String.valueOf(i));
-            }
-        }
-        return result;
-    }
+// class Solution {
+//     public List<String> fizzBuzz(int n) {
+//         List<String> result = new ArrayList<>();
+//         for (int i = 1; i <= n; i++) {
+//             if (i % 3 == 0 && i % 5 == 0) {
+//                 result.add("FizzBuzz");
+//             } else if (i % 3 == 0) {
+//                 result.add("Fizz");
+//             } else if (i % 5 == 0) {
+//                 result.add("Buzz");
+//             } else {
+//                 result.add(String.valueOf(i));
+    //         }
+    //     }
+    //     return result;
+    // }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("n = ");
-        int n = sc.nextInt();
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("n = ");
+    //     int n = sc.nextInt();
         
-        Solution solution = new Solution();
-        List<String> result = solution.fizzBuzz(n);
+    //     Solution solution = new Solution();
+    //     List<String> result = solution.fizzBuzz(n);
 
-        for (String s : result) {
-            System.out.print(s);
-            System.out.print(",");
-        }
-    }
-}
+    //     for (String s : result) {
+    //         System.out.print(s);
+    //         System.out.print(",");
+        // }
+    // }
+// }
